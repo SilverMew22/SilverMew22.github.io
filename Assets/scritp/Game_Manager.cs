99c3a482d8fc1435e1b7a1367ffe67a929d8625b
SilverMew22 <47192158+SilverMew22@users.noreply.github.com>
2019-04-08 20:09:58 -0400

    update
    animation

---
+++ Assets/scritp/Game_Manager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Manager : MonoBehaviour
+{
+    void EndGame() {
+        Debug.Log("Game Over");
+    }
+}
