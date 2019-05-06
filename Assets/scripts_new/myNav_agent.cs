34292daef8c89042efcb3aaf9cde125c0dbfa7ed
SilverMew22 <47192158+SilverMew22@users.noreply.github.com>
2019-05-06 18:58:37 -0400

    update

---
+++ Assets/scripts_new/myNav_agent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class myNav_agent : MonoBehaviour
+{
+    NavMeshAgent nav;
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nav.destination = target.position;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"));
+        { nav.destination=other.transform.position; }
+    }
+}
